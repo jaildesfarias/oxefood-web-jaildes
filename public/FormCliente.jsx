947f327d32d0e007f24214f73418316036c6e360
jaildesfarias <131688620+jaildesfarias@users.noreply.github.com>
2025-04-16 08:14:26 -0300

    Update FormCliente.jsx
    da uma revisão para ver se  esta certo

--- public/FormCliente.jsx
+++ public/FormCliente.jsx
@@ -1,125 +1,91 @@
-import React from 'react'
-import { Pagination } from 'semantic-ui-react'
-import PaginationExamplePagination from './components/PaginationExamplePagination';
-
+import React from 'react';
+import { Button, Container, Divider, Form, Icon, Segment } from 'semantic-ui-react';
 import InputMask from 'comigo-tech-react-input-mask';
-import React from "react";
-import { Button, Container, Divider, Form, Icon } from 'semantic-ui-react';
-import { Pagination } from 'semantic-ui-react'
-
-import { Segment } from 'semantic-ui-react';
 import './App.css';
-import FormCliente from './views/cliente/FormCliente';
 
-
-export default function FormCliente () {
-
+export default function CadastroCliente() {
     return (
+        <div style={{ marginTop: '3%' }}>
+            <Container textAlign='justified'>
+                <h2>
+                    <span style={{ color: 'darkgray' }}>
+                        Cliente &nbsp;<Icon name='angle double right' size="small" />
+                    </span> Cadastro
+                </h2>
 
-        <div>
+                <Divider />
 
-            <div style={{marginTop: '3%'}}>
+                <div style={{ marginTop: '4%' }}>
+                    <Form>
+                        <Form.Group widths='equal'>
+                            <Form.Input
+                                required
+                                fluid
+                                label='Nome'
+                                maxLength="100"
+                            />
 
-                <Container textAlign='justified' >
-
-                    <h2> <span style={{color: 'darkgray'}}> Cliente &nbsp;<Icon name='angle double right' size="small" /> </span> Cadastro </h2>
-
-                    <Divider />
-
-                    <div style={{marginTop: '4%'}}>
-
-                        <Form>
-
-                            <Form.Group widths='equal'>
-
-                                <Form.Input
-                                    required
-                                    fluid
-                                    label='Nome'
-                                    maxLength="100"
+                            <Form.Field required fluid>
+                                <label>CPF</label>
+                                <InputMask
+                                    mask="999.999.999-99"
                                 />
+                            </Form.Field>
+                        </Form.Group>
 
-                                <Form.Input
-                                    required
-                                    fluid
-                                    label='CPF'>
-                                    <InputMask
-                                        required
-                                        mask="999.999.999-99"
-                                    /> 
-                                </Form.Input>
+                        <Form.Group>
+                            <Form.Field fluid width={6}>
+                                <label>Fone Celular</label>
+                                <InputMask
+                                    mask="(99) 9999.9999"
+                                />
+                            </Form.Field>
 
-                            </Form.Group>
-                            
-                            <Form.Group>
+                            <Form.Field fluid width={6}>
+                                <label>Fone Fixo</label>
+                                <InputMask
+                                    mask="(99) 9999.9999"
+                                />
+                            </Form.Field>
 
-                                <Form.Input
-                                    fluid
-                                    label='Fone Celular'
-                                    width={6}>
-                                    <InputMask 
-                                        mask="(99) 9999.9999"
-                                    /> 
-                                </Form.Input>
+                            <Form.Field fluid width={6}>
+                                <label>Data Nascimento</label>
+                                <InputMask
+                                    mask="99/99/9999"
+                                    maskChar={null}
+                                    placeholder="Ex: 20/03/1985"
+                                />
+                            </Form.Field>
+                        </Form.Group>
+                    </Form>
 
-                                <Form.Input
-                                    fluid
-                                    label='Fone Fixo'
-                                    width={6}>
-                                    <InputMask 
-                                        mask="(99) 9999.9999"
-                                    /> 
-                                </Form.Input>
+                    <div style={{ marginTop: '4%' }}>
+                        <Button
+                            type="button"
+                            inverted
+                            circular
+                            icon
+                            labelPosition='left'
+                            color='orange'
+                        >
+                            <Icon name='reply' />
+                            Voltar
+                        </Button>
 
-                                <Form.Input
-                                    fluid
-                                    label='Data Nascimento'
-                                    width={6}
-                                >
-                                    <InputMask 
-                                        mask="99/99/9999" 
-                                        maskChar={null}
-                                        placeholder="Ex: 20/03/1985"
-                                    /> 
-                                </Form.Input>
-
-                            </Form.Group>
-                        
-                        </Form>
-                        
-                        <div style={{marginTop: '4%'}}>
-
-                            <Button
-                                type="button"
-                                inverted
-                                circular
-                                icon
-                                labelPosition='left'
-                                color='orange'
-                            >
-                                <Icon name='reply' />
-                                Voltar
-                            </Button>
-                                
-                            <Button
-                                inverted
-                                circular
-                                icon
-                                labelPosition='left'
-                                color='blue'
-                                floated='right'
-                            >
-                                <Icon name='save' />
-                                Salvar
-                            </Button>
-
-                        </div>
-
+                        <Button
+                            inverted
+                            circular
+                            icon
+                            labelPosition='left'
+                            color='blue'
+                            floated='right'
+                        >
+                            <Icon name='save' />
+                            Salvar
+                        </Button>
                     </div>
-                    
-                </Container>
-            </div>
+                </div>
+            </Container>
         </div>
-
     );
-
+}
