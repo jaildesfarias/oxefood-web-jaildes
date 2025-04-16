effc13685f42528ab5d2320b2516ce2e2f87aa85
jaildesfarias <131688620+jaildesfarias@users.noreply.github.com>
2025-04-16 01:23:33 -0300

    Update FormEntergador.jsx
    exercício

--- src/views/entergador/FormEntergador.jsx
+++ src/views/entergador/FormEntergador.jsx
@@ -1,116 +1,140 @@
-import InputMask from 'comigo-tech-react-input-mask';
-import React from "react";
+import React, { useState } from 'react';
 import { Button, Container, Divider, Form, Icon } from 'semantic-ui-react';
 
-export default function FormEntergador() {
+export default function FormProduto() {
+  const [ativo, setAtivo] = useState('');
 
-    return (
+  const handleAtivoChange = (e, { value }) => {
+    setAtivo(value);
+  };
 
-        <div>
+  return (
+    <div>
+      <div style={{ marginTop: '3%' }}>
+        <Container textAlign='justified'>
+          <h2>
+            <span style={{ color: 'darkgray' }}>
+              Cliente &nbsp;<Icon name='angle double right' size="small" />
+            </span>
+            Produto
+          </h2>
 
-            <div style={{marginTop: '3%'}}>
+          <Divider />
 
-                <Container textAlign='justified' >
+          <div style={{ marginTop: '4%' }}>
+            <Form>
 
-                    <h2> <span style={{color: 'darkgray'}}> Cliente &nbsp;<Icon name='angle double right' size="small" /> </span> Cadastro </h2>
+              {/* Grupo: Nome, Código e Ativo */}
+              <Form.Group widths='equal'>
+                <Form.Input
+                  fluid
+                  label='Nome'
+                  placeholder='Digite o nome'
+                />
+                <Form.Input
+                  fluid
+                  label='Código'
+                  placeholder='Digite o código'
+                />
+                <Form.Field>
+                  <label><strong>Ativo</strong></label>
+                  <Form.Group inline>
+                    <Form.Radio
+                      label='Sim'
+                      name='ativo'
+                      value='sim'
+                      checked={ativo === 'sim'}
+                      onChange={handleAtivoChange}
+                    />
+                    <Form.Radio
+                      label='Não'
+                      name='ativo'
+                      value='nao'
+                      checked={ativo === 'nao'}
+                      onChange={handleAtivoChange}
+                    />
+                  </Form.Group>
+                </Form.Field>
+              </Form.Group>
 
-                    <Divider />
+              {/* Grupo: Título e Código do Produto */}
+              <Form.Group widths='equal'>
+                <Form.TextArea
+                  label='Título'
+                  placeholder='Informe o título do produto'
+                />
+                <Form.TextArea
+                  label='Código do Produto'
+                  placeholder='Informe o código do produto'
+                />
+              </Form.Group>
 
-                    <div style={{marginTop: '4%'}}>
 
-                        <Form>
+              <Form.Group widths='equal'>
+                <Form.TextArea
+                  label='Descrição'
+                  placeholder='Informe descrição do produto'
+                />
+              </Form.Group>
 
-                            <Form.Group widths='equal'>
+              <Form.Group widths='equal'>
+                <Form.Input
+                  required
+                  fluid
+                  label='Valor Unitário'
+                  maxLength="100"
+                />
+              </Form.Group>
 
-                                <Form.Input
-                                    required
-                                    fluid
-                                    label='Nome'
-                                    maxLength="100"
-                                />
+              {/* Grupo: Tempo de Entrega Mínimo */}
+              <Form.Group widths='equal'>
+                <Form.Input
+                  type="number"
+                  label='Tempo de Entrega Mínimo (em Minutos)'
+                  placeholder='30'
+                />
+              </Form.Group>
 
-                                <Form.Input
-                                    required
-                                    fluid
-                                    label='CPF'>
-                                    <InputMask
-                                        required
-                                        mask="999.999.999-99"
-                                    /> 
-                                </Form.Input>
+              {/* Grupo: Tempo de Entrega Máximo */}
+              <Form.Group widths='equal'>
+                <Form.Input
+                  type="number"
+                  label='Tempo de Entrega Máximo (em Minutos)'
+                  placeholder='40'
+                />
+              </Form.Group>
 
-                            </Form.Group>
-                            
-                            <Form.Group>
+              <div style={{ marginTop: '4%' }}>
+                  
+                <Button
+                  type="button"
+                  inverted
+                  circular
+                  icon
+                  labelPosition='left'
+                  color='orange'
+                >
+                  <Icon name='arrow left' />
+                  Voltar
+                </Button>
 
-                                <Form.Input
-                                    fluid
-                                    label='Fone Celular'
-                                    width={6}>
-                                    <InputMask 
-                                        mask="(99) 9999.9999"
-                                    /> 
-                                </Form.Input>
+                <Button
+                  inverted
+                  circular
+                  icon
+                  labelPosition='left'
+                  color='blue'
+                  floated='right'
+                >
+                  <Icon name='check' />
+                  Salvar
+                </Button>
+              </div>
 
-                                <Form.Input
-                                    fluid
-                                    label='Fone Fixo'
-                                    width={6}>
-                                    <InputMask 
-                                        mask="(99) 9999.9999"
-                                    /> 
-                                </Form.Input>
-
-                                <Form.Input
-                                    fluid
-                                    label='Data Nascimento'
-                                    width={6}
-                                >
-                                    <InputMask 
-                                        mask="99/99/9999" 
-                                        maskChar={null}
-                                        placeholder="Ex: 20/03/1985"
-                                    /> 
-                                </Form.Input>
-
-                            </Form.Group>
-                        
-                        </Form>
-                        
-                        <div style={{marginTop: '4%'}}>
-
-                            <Button
-                                type="button"
-                                inverted
-                                circular
-                                icon
-                                labelPosition='left'
-                                color='orange'
-                            >
-                                <Icon name='reply' />
-                                Voltar
-                            </Button>
-                                
-                            <Button
-                                inverted
-                                circular
-                                icon
-                                labelPosition='left'
-                                color='blue'
-                                floated='right'
-                            >
-                                <Icon name='save' />
-                                Salvar
-                            </Button>
-
-                        </div>
-
-                    </div>
-                    
-                </Container>
-            </div>
-        </div>
-
-    );
-
+            </Form>
+          </div>
+        </Container>
+      </div>
+    </div>
+  );
 }
