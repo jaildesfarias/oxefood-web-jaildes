c6b8776675f052236a53bc1da541932d8fecbd97
jaildesfarias <131688620+jaildesfarias@users.noreply.github.com>
2025-04-15 22:15:41 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import { Segment } from 'semantic-ui-react';
 import './App.css';
+import Home from './views/home/Home';
 import FormProduto from './views/produto/FormProduto';
+
+import { ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+
+import FormCliente from './views/cliente/FormCliente';
 
 function App() {
 
