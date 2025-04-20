26a6b1dab6f5f6ae975e5ad0889314881c6ad951
jaildesfarias <131688620+jaildesfarias@users.noreply.github.com>
2025-04-19 23:12:50 -0300

    Update FormEntergador.jsx
    FormEntregador.jsx

--- src/views/entergador/FormEntergador.jsx
+++ src/views/entergador/FormEntergador.jsx
@@ -32,7 +32,6 @@ export default function FormProduto() {
 
           <div style={{ marginTop: '4%' }}>
             <Form>
-
               <Form.Group widths='equal'> {/* Grupo: Nome, Cpf e Rg */}
                 <Form.Input
                   fluid
@@ -50,8 +49,6 @@ export default function FormProduto() {
                   placeholder=''
                 />
               </Form.Group>
-              
-              {/* Grupo: Título e Código do Produto */}
               <Form.Group widths='equal'>
                 <Form.Input
                   fluid
@@ -78,13 +75,13 @@ export default function FormProduto() {
                   label='Valor Por Frete'
                   placeholder=''
                 />
- </Form.Group>
+             </Form.Group>
                 
               <Form.Group widths='equal'>
                 <Form.Input
                   fluid
                   label='Rua '
-                  placeholder='Informe descrição do produto'
+                  placeholder=''
                 />
                 <Form.TextArea
                   label='Número '
@@ -97,18 +94,12 @@ export default function FormProduto() {
                   required
                   fluid
                   label='Bairro'
-                  maxLength="100"
+                  maxLength=""
                 />
                  <Form.Input
                   required
                   fluid
                   label='Cidade'
-                  maxLength="100"
-                />
-                <Form.Input
-                  required
-                  fluid
-                  label='CEP'
                   maxLength=""
                 />
               </Form.Group>
@@ -123,34 +114,160 @@ export default function FormProduto() {
               </Form.Group>
 
               <Form.Group widths='equal'>
-                <Form.Input
                  <Form.Input
                   fluid
                   label='Complemento'
                   placeholder=''
                 />
               </Form.Group>
+               <Form.Group widths='equal'>
                    <Form.Field>
-                  <label><strong>Ativo</strong></label>
+                  
                   <Form.Group inline>
                     <Form.Radio
-
                       value='Ativo'
-                      checked={ativo === 'sim'}
+                      checked={ativo === 'import React, { useState } from 'react';
+import { Button, Container, Divider, Form, Icon } from 'semantic-ui-react';
+import React, { useState } from "react";
+
+export default function FormProduto() {
+  
+  const [nome, setNome] = useState();
+   const [cpf, setCpf] = useState();
+   const [rg, setRg] = useState();
+   const [dataNascimento, setDataNascimento] = useState();
+   const [foneCelular, setFoneCelular] = useState();
+   const [foneFixo, setFoneFixo] = useState();
+
+  const [ativo, setAtivo] = useState('');
+
+  const handleAtivoChange = (e, { value }) => {
+    setAtivo(value);
+  };
+
+  return (
+    <div>
+      <div style={{ marginTop: '3%' }}>
+        <Container textAlign='justified'>
+          <h2>
+            <span style={{ color: 'darkgray' }}>
+              Entrgador &nbsp;<Icon name='angle double right' size="small" />
+            </span>
+            Cadastro
+          </h2>
+
+          <Divider />
+
+          <div style={{ marginTop: '4%' }}>
+            <Form>
+              <Form.Group widths='equal'> {/* Grupo: Nome, Cpf e Rg */}
+                <Form.Input
+                  fluid
+                  label='Nome'
+                  placeholder=''
+                />
+                <Form.Input
+                  fluid
+                  label='CFP'
+                  placeholder=''
+                />
+                <Form.Input
+                  fluid
+                  label='RG'
+                  placeholder=''
+                />
+              </Form.Group>
+              
+              <Form.Group widths='equal'>
+                <Form.Input
+                  fluid
+                  label='DTNascimento'
+                  placeholder='20/03/1985'
+                />
+                <Form.Input
+                  fluid
+                  label='Fone Celular'
+                  placeholder=''
+                />
+                <Form.Input
+                  fluid
+                  label='Fone Fixo'
+                  placeholder=''
+                />
+               <Form.Input
+                  fluid
+                  label='QTD Entregas Realizadas'
+                  placeholder=''
+                />
+                <Form.Input
+                  fluid
+                  label='Valor Por Frete'
+                  placeholder=''
+                />
+             </Form.Group>
+                
+              <Form.Group widths='equal'>
+                <Form.Input
+                  fluid
+                  label='Rua '
+                  placeholder=''
+                />
+                <Form.TextArea
+                  label='Número '
+                  placeholder=''
+                />
+              </Form.Group>
+
+              <Form.Group widths='equal'>
+                <Form.Input
+                  required
+                  fluid
+                  label='Bairro'
+                  maxLength=""
+                />
+                 <Form.Input
+                  required
+                  fluid
+                  label='Cidade'
+                  maxLength=""
+                />
+              </Form.Group>
+
+              <Form.Group widths='equal'>
+                 <Form.Input
+                  required
+                  fluid
+                  label='UP'
+                  placeholder='Selecione'
+                />
+              </Form.Group>
+
+              <Form.Group widths='equal'>
+                 <Form.Input
+                  fluid
+                  label='Complemento'
+                  placeholder=''
+                />
+              </Form.Group>
+               <Form.Group widths='equal'>
+                   <Form.Field>
+                  
+                  <Form.Group inline>
+                    <Form.Radio
+                      value='Ativo'
+                      checked={ativo === 'Ativo'}
                       onChange={handleAtivoChange}
                     />
                     <Form.Radio
                       label='Sim'
-                     
                       value='sim'
-                      checked={ativo === 'sim'}
+                      checked={ativo === 'Sim'}
                       onChange={handleAtivoChange}
                     />
                     <Form.Radio
                       label='Não'
-                      
                       value='nao'
-                      checked={ativo === 'nao'}
+                      checked === 'Não'}
                       onChange={handleAtivoChange}
                     />
                   </Form.Group>
@@ -189,3 +306,8 @@ export default function FormProduto() {
     </div>
   );
 }
+
+                     
+              
+
+            
