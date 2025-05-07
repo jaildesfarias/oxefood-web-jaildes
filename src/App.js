aad34566e96eac909c06a3129aab7516997b3673
Jaildes Silva <aluno@ifpe>
2025-05-06 21:46:02 -0300

    Implementando a Alteração

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ function App() {
 
    </div>
  );
+ 
 }
 
 export default App;
--- src/Rotas.jsx
+++ src/Rotas.jsx
@@ -1,27 +1,32 @@
- import React from 'react';
+import React from 'react';
 import { Route, Routes } from "react-router-dom";
 
+// Importação correta dos componentes
 import FormCliente from './views/cliente/FormCliente';
-import FormEntregador from './views/entergador/FormEntergador';
+import ListCliente from './views/cliente/ListCliente';
+
+import FormEntregador from './views/entregador/FormEntregador';
+import ListEntregador from './views/entregador/ListEntregador';
+
+import DetalheEntregador from "./views/home/DetalheEntregador";
+
 import Home from './views/home/Home';
 import FormProduto from './views/produto/FormProduto';
-
-import ListCliente from './views/cliente/ListCliente';
+import ListProduto from './views/produto/ListProduto';
 
 function Rotas() {
     return (
-        <>
-            <Routes>
-                <Route path="/" element={ <Home/> } />
-                <Route path="form-cliente" element={ <FormCliente/> } />
-                <Route path="form-produto" element={ <FormProduto/> } />
-                <Route path="form-entregador" element={ <FormEntregador/> } />
-
-                <Route path="list-cliente" element={ <ListCliente/> } />
-
-            </Routes>
-        </>
-    )
+        <Routes>
+            <Route path="/" element={ <Home/> } />
+            <Route path="form-cliente" element={ <FormCliente/> } />
+            <Route path="form-produto" element={ <FormProduto/> } />
+            <Route path="form-entregador" element={ <FormEntregador/> } />
+            <Route path="list-cliente" element={ <ListCliente/> } />
+            <Route path="list-produto" element={ <ListProduto/> } />
+            <Route path="list-entregador" element={ <ListEntregador/> } />
+            <Route path="detalhe-entregador/:id" element={ <DetalheEntregador/> } />
+        </Routes>
+    );
 }
 
 export default Rotas;
